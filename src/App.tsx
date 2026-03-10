ccad123ccb1cf9d8a9e00f12c3571cdf4ae58ec8
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-10 12:12:58 -0300

    feat(feature/app-layout): criado um button reutilizavel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Header } from "./components/header/Header"
+import { Button } from "./components/button/Button"
+
 function App() {
   return (
     <div>
@@ -7,6 +9,9 @@ function App() {
         <h1>Bem-vindo ao App dos Dragões!</h1>
         <p>Esta é a página principal da aplicação.</p>
       </main>
+      <Button variante="primary" onClick={() => console.log("Reutilizado!")}>
+        Click me para reutilizar!
+        </Button>
     </div>
   )
 }
