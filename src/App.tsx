78a7fa3fc1c8f151c37b2fb7e3b82981200788a4
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-10 11:42:21 -0300

    feat(feature-layout): criado header inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Header } from "./components/header/Header"
 function App() {
-  return <h1>Dragons App</h1>
+  return (
+    <div>
+      <Header />
+      <main className="container">
+        <h1>Bem-vindo ao App dos Dragões!</h1>
+        <p>Esta é a página principal da aplicação.</p>
+      </main>
+    </div>
+  )
 }
 
 export default App
