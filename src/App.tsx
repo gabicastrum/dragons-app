e2fe3364c0ffda609c6e7e7e2e3b09c3a1ed5385
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-11 11:44:36 -0300

    feat(feature/auth-routes): adicionado autenticacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './page/login/LoginPage';
 import ListPage from './page/list/ListPage';
+import { AuthProvider } from './contexts/AuthProvider';
+import { PrivateRoute } from './routes/PrivateRoutes';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <AuthProvider>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/dragoes" element={<ListPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/dragoes" element={ <PrivateRoute><ListPage /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
