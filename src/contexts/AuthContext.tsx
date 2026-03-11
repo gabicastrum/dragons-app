77adae1db76229673626429dc077315f358b998f
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-11 11:44:36 -0300

    feat(feature/auth-routes): adicionado autenticacao

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+export interface AuthContextData {
+  logado: boolean;
+  login: (email: string, password: string) => boolean;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextData | null>(null);
