6deba082d0a6a3b3d8e1c1a1d3e560aa8d988c48
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-13 14:50:35 -0300

    fix(PROD): correção url

--- src/services/dragonService.ts
+++ src/services/dragonService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { type IDragon } from "../interfaces/dragon";
-const API_URL = "http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon";
+const API_URL = "https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon";
 
 export const listarDragoes = async (): Promise<IDragon[]> => {
   const response = await axios.get(API_URL);
