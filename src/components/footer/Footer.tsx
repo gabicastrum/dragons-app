0b52b95eb00aa05fd05d4298076feeced775dad4
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-13 15:35:37 -0300

    refactor(refactor/melhorias): incluindo autoria com footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      © 2026 · Autoria de{" "}
+      <a
+        href="https://www.linkedin.com/in/gabrieladecastrolaurindo/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Gabriela de Castro
+      </a>
+    </footer>
+  );
+}
